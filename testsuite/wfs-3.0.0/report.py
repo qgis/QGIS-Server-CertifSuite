3905d03133f0c2b40d448edfa227d9ebf5a4a300
Blottiere Paul <blottiere.paul@gmail.com>
2019-10-28 19:50:07 +0100

    Add basic html report with toc

--- testsuite/wfs-3.0.0/report.py
+++ testsuite/wfs-3.0.0/report.py
@@ -1,11 +1,136 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import argparse
-import subprocess
-import datetime
 import os
 import shutil
+import argparse
+import datetime
+import subprocess
+import xml.etree.ElementTree as ET
+
+
+class Class(object):
+
+    def __init__(self, name):
+        self.name = name.split('.')[-1]
+        self.link = "https://opengeospatial.github.io/ets-wfs30/apidocs/{}".format(name.replace('.', '/'))
+        self.methods = []
+
+    def dump(self):
+        print("Class: ")
+        print("  - name: {}".format(self.name))
+        print("  - link: {}".format(self.link))
+        print("  - passed: {}".format(self.status()))
+
+        for m in self.methods:
+            m.dump()
+
+    def status(self):
+
+        passed = True
+        for m in self.methods:
+            passed &= m.passed()
+
+        if passed:
+            return "Passed"
+        else:
+            return "Fail"
+
+    def color(self):
+        if self.status() == "Passed":
+            return "#006600"
+        else:
+            return "#e60000"
+
+    def toc(self):
+        href = ('<a href="#">{}</a><b style="font-family: Verdana, '
+                'sans-serif; color: {};"> {}</b>'
+               ).format(self.name, self.color(), self.status())
+
+        t = ''
+        for m in self.methods:
+            t += m.toc()
+
+        toc = ('<ul>\n'
+           '  <li>\n'
+           '    {0}\n'
+           '    {1}\n'
+           '  </li>\n'
+           '</ul>').format(href, t)
+
+        return toc
+
+
+class Method(object):
+
+    def __init__(self, name, status):
+        self.name = name
+
+        self.status = "Passed"
+        if status == "FAIL":
+            self.status = "Failed"
+        elif status == "SKIP":
+            self.status = "Skipped"
+
+    def dump(self):
+        print("    - {}: {}".format(self.name, self.status))
+
+    def passed(self):
+        if self.status == 'FAIL':
+            return False
+        else:
+            return True
+
+    def color(self):
+        if self.status == "Passed":
+            return "#006600"
+        elif self.status == "Skipped":
+            return "#ffa500"
+        else:
+            return "#e60000"
+
+    def toc(self):
+        href = ('<a href="#">{}</a><b style="font-family: Verdana, '
+                'sans-serif; color: {};"> {}</b>'
+               ).format(self.name, self.color(), self.status)
+
+        toc = ('<ul>\n'
+           '  <li>\n'
+           '    {0}\n'
+           '  </li>\n'
+           '</ul>').format(href)
+
+        return toc
+
+
+class Toc(object):
+
+    def __init__(self, classes):
+        self.classes = classes
+
+    def toc(self):
+        toc = ''
+
+        for cl in self.classes:
+            toc += cl.toc()
+
+        return toc
+
+    def status(self):
+
+        status = "Passed"
+
+        for cl in self.classes:
+            if cl.status != "Passed":
+                status = "Failed"
+
+        return status
+
+    def color(self):
+        if self.status() == "Passed":
+            return "#006600"
+        else:
+            return "#e60000"
 
 
 def run_cmd(cmd):
@@ -25,42 +150,61 @@ def run_teamengine(xml, teamengine_url, getcapabilities_url):
     out, err = run_cmd('wget --header="Accept: application/xml" -O {} {}'.format(xml, getcapa))
 
 
-def generate_html(xml, html, version, commit):
-    out, err = run_cmd(['xmlstarlet', 'tr', 'tohtml.xsl', xml])
-    out = out.decode("utf-8")
+def generate_html(xml, outdir, version, commit):
+    # parse xml
+    tree = ET.parse(xml)
+    tests = tree.find('suite').find('test')
 
-    report_out = ''
+    classes = []
+    for test in tests:
+        cl = Class(test.attrib['name'])
+        for ch in test:
+            m = Method(ch.attrib['name'], ch.attrib['status'])
+            cl.methods.append(m)
+        classes.append(cl)
 
-    for line in out.splitlines(keepends=True):
-        # clear line
-        line = line.replace('&quot;', '"')
-        line = line.replace('&amp;', '&')
+    toc = Toc(classes)
 
-        # date
-        date_tag = '{{TEMPLATE_DATE}}'
-        if date_tag in line:
-            format = '%Y-%m-%d %H:%M:%S'
-            date = datetime.datetime.now().strftime(format)
-            line = date
+    # generate html
+    outpath = os.path.join(outdir, 'report.html')
+    with open('template.html', 'r') as infile:
+        with open(outpath, 'w') as outfile:
+            for line in infile:
+                # date
+                date_tag = '{{TEMPLATE_DATE}}'
+                if date_tag in line:
+                    format = '%Y-%m-%d %H:%M:%S'
+                    date = datetime.datetime.now().strftime(format)
+                    line = date
 
-        # version
-        version_tag = '{{TEMPLATE_VERSION}}'
-        if version_tag in line:
-            line = line.replace(version_tag, version)
+                # overall result
+                color_tag = '{{TEMPLATE_RESULT_COLOR}}'
+                status_tag = '{{TEMPLATE_RESULT_STATUS}}'
+                if color_tag in line:
+                    line = line.replace(color_tag, toc.color())
+                    line = line.replace(status_tag, toc.status())
 
-        # commit
-        commit_tag = '{{TEMPLATE_COMMIT}}'
-        if commit_tag in line:
-            commit_link = 'https://github.com/qgis/QGIS/commit/{}'.format(commit)
-            commit_link_tag = '%7BTEMPLATE_COMMIT_LINK%7D'
-            line = line.replace(commit_link_tag, commit_link)
-            line = line.replace(commit_tag, commit)
+                # version
+                version_tag = '{{TEMPLATE_VERSION}}'
+                if version_tag in line:
+                    line = line.replace(version_tag, version)
 
-        report_out += line
+                # commit
+                commit_tag = '{{TEMPLATE_COMMIT}}'
+                if commit_tag in line:
+                    line = line.replace(commit_tag, commit)
 
-    f = open(html, 'w')
-    f.write(report_out)
-    f.close()
+                # toc
+                toc_tag = '{{TEMPLATE_TOC}}'
+                if toc_tag in line:
+                    line = toc.toc()
+
+                # # body
+                # body_tag = '{{TEMPLATE_BODY}}'
+                # if body_tag in line:
+                #     line = cont
+
+                outfile.write(line)
 
 
 if __name__ == '__main__':
@@ -85,8 +229,8 @@ if __name__ == '__main__':
     html = '{}/report.html'.format(outdir)
     log = '{}/log.txt'.format(outdir)
 
-    clean(outdir)
-    run_teamengine(xml, teamengine_url, getcapabilities_url)
-    generate_html(xml, html, args.version, args.hash)
+    # clean(outdir)
+    # run_teamengine(xml, teamengine_url, getcapabilities_url)
+    generate_html(xml, outdir, args.version, args.hash)
     shutil.copy('style.css', outdir)
     shutil.copy('logo.png', outdir)
