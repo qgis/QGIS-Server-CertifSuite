757771611ad2aecff8238f5a9f53701d194d9974
Blottiere Paul <blottiere.paul@gmail.com>
2021-11-30 21:35:28 +0100

    Update docker build

--- docker/master/build.sh
+++ docker/master/build.sh
@@ -1,10 +1,4 @@
 #! /bin/sh
 
 BASEDIR=$(dirname "$0")
-docker build ${BASEDIR} -t qgisserver-certifsuite/master-prepare
-docker run --name certifsuite-master-build --rm --privileged -d -it qgisserver-certifsuite/master-prepare /bin/bash
-docker exec certifsuite-master-build sh /root/qgis.sh
-docker commit --change='CMD ["sh", "/root/cmd.sh"]' certifsuite-master-build qgisserver-certifsuite/master
-docker stop certifsuite-master-build
-sleep 5
-docker rmi qgisserver-certifsuite/master-prepare
+docker build ${BASEDIR} -t qgisserver-certifsuite/master
